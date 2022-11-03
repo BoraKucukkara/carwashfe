a0519affa13ba4cb703bd443bab473281700959c
BoraKucukkara <89361281+BoraKucukkara@users.noreply.github.com>
2022-11-03 13:54:34 +0300

    npm router sass

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { routes } from "./routes/routes";
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes, 
+  mode : 'history' 
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/routes/routes.js
@@ -0,0 +1,7 @@
+import Home from "../components/homeView";
+import User from "../components/userView";
+
+export const routes = [
+    { path : '/', component : Home  }, 
+    { path: '/user', component: User}
+];
